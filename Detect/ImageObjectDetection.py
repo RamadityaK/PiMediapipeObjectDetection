09d9f0d7ce36657e6584fd0728cad32865e6b866
RamadityaK <adi.k394@yahoo.com>
2024-07-22 10:28:14 -0500

    Cleaned up Image Detection Code

--- Detect/ImageObjectDetection.py
+++ Detect/ImageObjectDetection.py
@@ -3,29 +3,40 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2
 import numpy as np
-#from utils import visualize
+import time
 
-IMAGE_FILE = 'pigImage.jpg'
+#Variables to benchmark inference performance
+startTime = 0
+inferenceTime = 0
 
-base_options = python.BaseOptions(model_asset_path = 'test.tflite')
+# You can import any jpg image and change this filename to test it out!
+IMAGE_FILE = 'image.jpg'
+
+#Configure the Mediapipe Object to perform inferencing
+base_options = python.BaseOptions(model_asset_path = 'efficientdet.tflite')
 options = vision.ObjectDetectorOptions(base_options=base_options,
                                        score_threshold=0.5)
 detector = vision.ObjectDetector.create_from_options(options)
 
+#Process the image into a format that Mediapipe likes
 image = mp.Image.create_from_file(IMAGE_FILE)
 
+#Run the inferencing
+startTime = time.time()
 detection_result = detector.detect(image)
-print(detection_result)
-print('-----------------------')
+inferenceTime = time.time() - startTime
+
+#You can ignore the below code, it's mainly used for debugging
+#print(detection_result)
+#print('-----------------------')
+
+#Parse through each object detected (ironically, detection is also an object, see the Mediapipe Documentation for more information)
 for detection in detection_result.detections:
-    category = detection.categories[0]
-    bbox = detection.bounding_box
+    category = detection.categories[0] #This line grabs the category of the detection with the highest probablity (This is an object)
+    bbox = detection.bounding_box # This line grabs the bounding box of the detection (This is an object)
+    #Display the results of the detection and misc. info.
     print(category.category_name, ", with prob: ", str(round(category.score,2))
           , "and centroid: ", str(bbox.origin_x+(bbox.width/2)),",",str(bbox.origin_y+(bbox.height/2)))
 
-image_copy = np.copy(image.numpy_view())
-# annotated_image = visualize(image_copy,detection_result)
-# rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-# cv2.imshow(rgb_annotated_image)
-
-cv2.destroyAllWindows()
+#Print the time it takes to perform inference on the image for benchmarking
+print('Inference Time: ',inferenceTime)
